ab96d59ccf24ebf30d5d5ba27d4680a8de020d6f
esobirjon2197 <esobirjon2197@gmail.com>
2026-02-18 16:47:24 +0500

    Add dictionary examples and operations in main.py
    Added examples of dictionaries for students, prices, cars, and a person. Included operations like printing and deleting a key.

---
+++ main.py
@@ -0,0 +1,53 @@
+# 1-m
+from idlelib.editor import keynames
+
+student = {"Ali": 85, "Vali": 90, "Hasan": 78}
+
+print(student)
+
+print(len(keynames))
+
+
+# 2-m
+student = {"Ali": 85,
+           "Vali": 90,
+           "Hasan": 78
+}
+
+print(student)
+
+print(max(student.values()))
+
+
+# 3-m
+prices = {"apple": 5000,
+          "banana": 3000,
+          "cherry": 7000
+}
+
+print(prices)
+
+
+# 4-m
+car = {"brand": "Chevrolet",
+                "year": 2022,
+                "color": "black"
+}
+
+print(car)
+
+
+# 5-m
+person = {"name": "Ali",
+          "age": 25,
+          "city": "Toshkent"
+}
+
+print(person)
+
+del person["age"]
+print(person)
+
+
+
+
